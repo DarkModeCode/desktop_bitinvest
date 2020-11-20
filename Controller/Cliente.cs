501a8388391a464b7d45b383b0ad639cd85c88c5
Vanessa Lemos <va.lemos.28@gmail.com>
2020-11-19 21:31:40 -0300

    ...

--- Controller/Cliente.cs
+++ Controller/Cliente.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace desktop_bitinvest_v1.Controller
 {
-   public class Cliente
-    {        public static string Id { get; set; }
+    public class Cliente
+    { public static string Id { get; set; }
         public static string Email { get; set; }
         public static string Senha { get; set; }
         public static string Nome { get; set; }
@@ -18,11 +18,11 @@ namespace desktop_bitinvest_v1.Controller
         public static string cpf_cnpj { get; set; }
         public static string telefone_residencial { get; set; }
         public static string celular { get; set; }
-        public static string RendaMensal { get; set; } 
+        public static string RendaMensal { get; set; }
         public static string Rua { get; set; }
         public static string Bairro { get; set; }
         public static string Complemento { get; set; }
-        public static string Cidade { get; set; } 
+        public static string Cidade { get; set; }
         public static string Numero { get; set; }
         public static string Estado { get; set; }
         public static string Pais { get; set; }
