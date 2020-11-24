0d02b13128bf284c244661d1f3af3c8e221d6d09
DarkModeCode <74278364+DarkModeCode@users.noreply.github.com>
2020-11-24 14:07:05 -0300

    Update Funcionario.cs

--- Controller/Funcionario.cs
+++ Controller/Funcionario.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace desktop_bitinvest_v1.Controller
 {
+//classe para salvar os dados do funcionario
     class Funcionario
     {
         public static int Id { get; set; }
